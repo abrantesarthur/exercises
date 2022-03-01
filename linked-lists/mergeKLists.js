c755fe513f6c4f9526990ccfd559014399ec8c52
Arthur Abrantes <abrantesarthur1997@gmail.com>
2022-03-01 00:17:55 -0500

    implement approach 2 to linked-lists/mergeKLists.js

--- linked-lists/mergeKLists.js
+++ linked-lists/mergeKLists.js
@@ -8,25 +8,68 @@ function ListNode(val, next) {
 
 var mergeKListsBruteForce = function (lists) {
   // merge all lists into single list
-  let answer = [];
+  let nodeList = [];
   for (head of lists) {
     let node = head;
     while (node) {
-      answer.push(node);
+      nodeList.push(node);
       node = node.next;
     }
   }
 
   // sort
-  answer.sort((a, b) => a.val - b.val);
+  nodeList.sort((a, b) => a.val - b.val);
 
   // re-link
-  for (let i = 0; i < answer.length - 1; i++) {
-    answer[i].next = answer[i + 1];
+  for (let i = 0; i < nodeList.length - 1; i++) {
+    nodeList[i].next = nodeList[i + 1];
   }
-  answer[answer.length - 1].next = null;
+  nodeList[nodeList.length - 1].next = null;
 
-  return answer;
+  return nodeList[0];
+};
+
+// Approach 2: compare 1 to 1
+var mergeKListCompare = function (lists) {
+  if (lists.length == 0) {
+    return null;
+  }
+
+  let answerHead = null;
+  let answerTail = null;
+
+  while (true) {
+    // get index of smallest node
+    let si;
+    for (let i = 0; i < lists.length; i++) {
+      if (si == undefined && lists[i] != null) {
+        si = i;
+      } else if (lists[i] != null && lists[i].val < lists[si].val) {
+        si = i;
+      }
+    }
+
+    // if all lists are empty, break out of the loop
+    if (lists[si] == null) {
+      break;
+    }
+
+    // remove smallest node from its list
+    let smallestHead = lists[si];
+    lists[si] = lists[si].next;
+    smallestHead.next = null;
+
+    // append smallest node to final answer
+    if (answerHead == null) {
+      answerHead = smallestHead;
+      answerTail = smallestHead;
+    } else {
+      answerTail.next = smallestHead;
+      answerTail = smallestHead;
+    }
+  }
+
+  return answerHead;
 };
 
 let list1Hd = new ListNode(0, null);
