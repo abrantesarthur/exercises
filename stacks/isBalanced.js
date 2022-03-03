ded8026441ad293c8f8203e45ce010de3ae12975
Arthur Abrantes <abrantesarthur1997@gmail.com>
2022-03-03 18:57:21 -0500

    finish stacks/isBalanced.js

---
+++ stacks/isBalanced.js
@@ -0,0 +1,44 @@
+// write a function that takes a string of brackets as input
+// and returns true if the brackets are balanced
+
+const bracketsMatch = function (opening, closing) {
+  if (opening == "(" && closing == ")") {
+    return true;
+  }
+  if (opening == "{" && closing == "}") {
+    return true;
+  }
+  if (opening == "[" && closing == "]") {
+    return true;
+  }
+  return false;
+};
+
+const isBalanced = function (s) {
+  if (s.length == 0) {
+    return true;
+  }
+  if (s.length % 2 != 0) {
+    return false;
+  }
+
+  let stack = [];
+
+  for (let i = 0; i < s.length / 2; i++) {
+    stack.push(s[i]);
+  }
+  for (let i = s.length / 2; i < s.length; i++) {
+    let opening = stack.pop();
+    if (bracketsMatch(opening, s[i]) == false) {
+      return false;
+    }
+  }
+  return true;
+};
+
+console.log(isBalanced(""));
+console.log(isBalanced("["));
+console.log(isBalanced("[}"));
+console.log(isBalanced("[]"));
+console.log(isBalanced("[({})]"));
+console.log(isBalanced("([({})]}"));
