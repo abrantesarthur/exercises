f46d1194bb45f60ce9173302d18bfa1693dfd6f5
Arthur Abrantes <abrantesarthur1997@gmail.com>
2022-03-03 20:05:51 -0500

    finish graphs/allPathsSourceTarget.js

--- graphs/allPathsSourceTarget.js
+++ graphs/allPathsSourceTarget.js
@@ -1,1 +1,27 @@
 // https://leetcode.com/problems/all-paths-from-source-to-target/
+
+const allPathsSourceTarget = function (graph) {
+  return allPaths(graph, 0).filter((p) => p.length > 0);
+};
+
+const allPaths = function (graph, i) {
+  let n = graph.length;
+  if (i == n - 1) {
+    return [[n - 1]];
+  }
+  if (graph[i].length == 0) {
+    return [[]];
+  }
+
+  let paths = [];
+  for (let j = 0; j < graph[i].length; j++) {
+    paths = [...paths, ...allPaths(graph, graph[i][j])];
+  }
+
+  for (let j = 0; j < paths.length; j++) {
+    if (paths[j].length > 0) {
+      paths[j].unshift(i);
+    }
+  }
+  return paths;
+};
