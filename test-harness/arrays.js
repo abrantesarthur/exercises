6977ce257f3e88951efc934170bb64d57cf97b73
Arthur Abrantes <abrantesarthur1997@gmail.com>
2022-03-06 11:50:16 -0500

    create test-harness folder

---
+++ test-harness/arrays.js
@@ -0,0 +1,33 @@
+function check(expected, output) {
+  var expected_size = expected.length;
+  var output_size = output.length;
+  var result = true;
+  if (expected_size != output_size) {
+    result = false;
+  }
+  for (var i = 0; i < Math.min(expected_size, output_size); i++) {
+    result &= output[i] == expected[i];
+  }
+  var rightTick = "\u2713";
+  var wrongTick = "\u2717";
+  if (result) {
+    var out = rightTick + " Test #" + test_case_number;
+    console.log(out);
+  } else {
+    var out = "";
+    out += wrongTick + " Test #" + test_case_number + ": Expected ";
+    out += printintegerArray(expected);
+    out += " Your output: ";
+    out += printintegerArray(output);
+    console.log(out);
+  }
+  test_case_number++;
+}
+
+var test_1 = [3, 4, 1, 6, 2];
+// should pass
+check([3, 4, 1, 6, 2], [3, 4, 1, 6, 2]);
+// should fail
+check([3, 4, 1, 6, 2], [3, 4, 1, 6]);
+// should fail
+check([3, 4, 1, 6, 2], [3, 4, 1, 6, 3]);
---
+++ test-harness/strings.js
@@ -0,0 +1,23 @@
+function check(expected, output) {
+  var result = expected == output;
+  var rightTick = "\u2713";
+  var wrongTick = "\u2717";
+  if (result) {
+    var out = rightTick + " Test #" + test_case_number;
+    console.log(out);
+  } else {
+    var out = "";
+    out += wrongTick + " Test #" + test_case_number + ": Expected ";
+    out += printString(expected);
+    out += " Your output: ";
+    out += printString(output);
+    console.log(out);
+  }
+  test_case_number++;
+}
+
+var str = "Epp-gsrzsCw-3-fi:Epivx5.";
+// should pass
+check(str, str);
+// should fail
+check(str, str + "a");
