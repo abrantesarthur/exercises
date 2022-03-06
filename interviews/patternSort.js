384a6e1d40625fccaab4959e9daa4476ef767b68
Arthur Abrantes <abrantesarthur1997@gmail.com>
2022-03-06 11:44:35 -0500

    finish facebook/patternSort.js

--- interviews/patternSort.js
+++ interviews/patternSort.js
@@ -1,1 +1,38 @@
 // https://www.geeksforgeeks.org/sort-string-according-order-defined-another-string/
+
+const sort = function (input, pattern) {
+  /// create hash table
+  let inputHashTable = {};
+  let patternHashTable = {};
+
+  // count character occurences in pattern
+  for (c of pattern) {
+    if (c in patternHashTable) {
+      patternHashTable[c]++;
+    } else {
+      patternHashTable[c] = 1;
+    }
+  }
+
+  let suffix = "";
+
+  //cout character occurrences in input
+  for (c of input) {
+    if (c in patternHashTable === false) {
+      suffix += c;
+    } else if (c in inputHashTable) {
+      inputHashTable[c]++;
+    } else {
+      inputHashTable[c] = 1;
+    }
+  }
+
+  let result = "";
+
+  for (c of pattern) {
+    result += c.repeat(inputHashTable[c]);
+  }
+  return result + suffix;
+};
+
+console.log(sort("aedbaeccebba", "bca"));
